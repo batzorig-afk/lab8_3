4c6673b2615a0aa7919e629f394966533f683c4c
ganz0rig <gdawaasvren309@gmail.com>
2022-12-05 15:00:53 +0800

    B200910022

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react'
+import {StyleSheet, Text, View} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component{
+  constructor(){
+    super()
+    this.state = {}
+  }
+  render(){
+    return(
+      <View style = {css.container}>
+        <View style={css.result}></View>
+        <View style={css.calculation}></View>
+        <View style={css.buttons}>
+          <View style={css.numbers}></View>
+          <View style={css.operations}></View>
+        </View>
+        <View style={css.new}></View>
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const css = StyleSheet.create({
+  container:{
+    flex: 1
   },
+  result:{
+    flex:2,
+    backgroundColor:'red'
+  },
+  calculation:{
+    flex:2,
+    backgroundColor:'green'
+  },
+  buttons:{
+    flex:7, 
+    flexDirection: 'row'
+  },
+  numbers:{
+    flex:3,
+    backgroundColor: 'yellow'
+  },
+  operations:{
+    flex: 1,
+    backgroundColor: 'black'
+  },
+  new:{
+    flex:1,
+    backgroundColor: "blue"
+  }
 });
